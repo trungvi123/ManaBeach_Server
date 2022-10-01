a1014cd3bdfb3d76e1f5eeb43c9a46d69df4d855
trungvi123 <trungvi123@gmail.com>
2022-10-01 22:29:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import env from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import adminRouter from "./routers/adminRoutes.js";
+import userRouter from "./routers/userRoutes.js";
+
+env.config();
+
+const app = express();
+let PORT = process.env.PORT || 4000; // add your port
+let URI = process.env.URI_BASE; // add your URI
+
+// ADMIN_KEY_SIGNUP = 'admin@admin.ctu.edu.vn' cần thêm adminKey khi thêm 1 admin
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors());
+app.use("/product", productRouter);
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:uh02hELNXTJYuW88@cluster0.ohupeby.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("SERVER RUN");
+    });
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ models/productModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema(
+    {
+    imgs: {
+        type:Array,
+        require:true
+    },
+    price: {
+        type:Number,
+        require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    type:{
+        type:Array,
+        require:true
+    },
+    description:{
+        type:String,
+        default:'<p><strong>Căn phòng chất lượng với tầm nhìn ra biển </strong></p><p> Luxury Double Room Suite có kích thước nữ hoàng hoặc giường đôi thoải mái, khu vực tiếp khách, bàn làm việc và phòng tắm riêng biệt với vòi hoa sen nhảy đi bộ hoặc bồn tắm và vòi sen cùng nghệ thuật hiện đại và màu sắc trung tính.</p>'
+    }
+},{timestamps:true})
+
+export const productModel = mongoose.model('product',schema)
